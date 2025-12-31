186c5a598ecd9c2d98240d09a25b3d6c079c624a
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2025-12-31 15:22:58 -0600

    Actualizacion modelo Empleado

--- Bank_Test/src/main/java/com/montelongo/Bank_Test/domain/Empleado.java
+++ Bank_Test/src/main/java/com/montelongo/Bank_Test/domain/Empleado.java
@@ -1,5 +1,6 @@
 package com.montelongo.Bank_Test.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,35 @@ import java.time.LocalDate;
 
 
 @Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "empleados")
 public class Empleado{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "numberPhone", unique = true)
+    private Long numberPhone;
+
+    @Column(name = "birthdate")
+    private String birthdate;
+
+    @Column(name = "numEmployee", unique = true)
     private String numEmployee;
+
+    @Column(name = "areaEmployee")
     private String areaEmployee;
-    private Float mouthlySalary;
-    private Integer salesNumber;
+
+    @Column(name="addressStore")
+    private String addressStore;
 
 }
